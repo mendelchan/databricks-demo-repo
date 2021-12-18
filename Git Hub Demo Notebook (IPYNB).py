74b6d3a87e0793b1c1d549dd2d8fa0481aef57ad
chan.mendelsohn.neil@gmail.com <chan.mendelsohn.neil@gmail.com>
2021-12-18 05:18:57 +0000

    4th commit

---
+++ Git Hub Demo Notebook (IPYNB).py
@@ -0,0 +1,17 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC 
+# MAGIC # This is Version 1 Ipynb version
+
+# COMMAND ----------
+
+print("Hello World")
+
+# COMMAND ----------
+
+for i in range(0, 5):
+  print(i)
+
+# COMMAND ----------
+
+
